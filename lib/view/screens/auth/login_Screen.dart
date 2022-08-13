8f922bd35656705de2954a1a1365c8a696e0c246
hardikpatel2728 <hardik.raghuveer@gmail.com>
2022-08-13 23:58:10 +0530

    tiktok clone

---
+++ lib/view/screens/auth/login_Screen.dart
@@ -0,0 +1,50 @@
+import 'package:clonetiktok/view/widget/text_input.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Person Vidz",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w900),),
+            SizedBox(height: 30,),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                  controller: emailController,
+                  myIcon: Icons.email,
+                  myLableText: "Email"),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: passwordController,
+                myIcon: Icons.lock,
+                myLableText: "Password",
+                toHide: true,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                    child: Text("Login")))
+          ],
+        ),
+      ),
+    );
+  }
+}
