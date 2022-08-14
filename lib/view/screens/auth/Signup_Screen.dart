cc02bc20543b0c660876216ee704ecb992190a74
hardikpatel2728 <hardik.raghuveer@gmail.com>
2022-08-15 00:29:19 +0530

    login screen

--- lib/view/screens/auth/Signup_Screen.dart
+++ lib/view/screens/auth/Signup_Screen.dart
@@ -1,13 +1,37 @@
+import 'dart:io';
 import 'package:clonetiktok/view/widget/text_input.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import '../../widget/glitch.dart';
 
-class SignupScreen extends StatelessWidget {
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  File? image;
   TextEditingController emailController = TextEditingController();
   TextEditingController usernameController = TextEditingController();
   TextEditingController setpasswordController = TextEditingController();
   TextEditingController confirmpasswordController = TextEditingController();
 
+  Future pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if(image == null) return;
+
+      final imageTemporary = File(image.path);
+      setState(() =>
+      this.image = imageTemporary);
+    } on PlatformException catch (e) {
+      print('Failed to pick image:$e');
+    }
+
+  }
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -22,32 +46,35 @@ class SignupScreen extends StatelessWidget {
               children: [
                 GlithEffect(
                     child: Text(
-                  "Welcome to Person Vidz ",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
-                )),
+                      "Welcome to Person Vidz ",
+                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+                    )),
                 SizedBox(
                   height: 30,
                 ),
                 Stack(
                   children: [
-                    const CircleAvatar(
-                      backgroundImage:NetworkImage("https://st3.depositphotos.com/1767687/16607/v/450/depositphotos_166074422-stock-illustration-default-avatar-profile-icon-grey.jpg") ,
-                      radius: 60,
+                    image !=null ?Image.file(image!) : InkWell(onTap: ()=>pickImage(ImageSource.gallery),
+                      child:
+                      CircleAvatar(
+                        backgroundImage:NetworkImage("https://st3.depositphotos.com/1767687/16607/v/450/depositphotos_166074422-stock-illustration-default-avatar-profile-icon-grey.jpg") ,
+                        radius: 60,
+                      ),
                     ),
                     Positioned(
                         bottom: 0,
                         right: 0,
                         child: Container(
-                          padding: EdgeInsets.all(2),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: Colors.white
-                          ),
+                            padding: EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.white
+                            ),
                             child: Icon(
-                          Icons.edit,
-                          size: 30,
+                              Icons.edit,
+                              size: 30,
                               color: Colors.black,
-                        )))
+                            )))
                   ],
                 ),
                 SizedBox(
